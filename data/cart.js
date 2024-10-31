e20fc7d54adb717e1c3d41998dcfa766967fe6f7
jaseel_js <jazeel1132@gmail.com>
2024-10-31 12:03:45 +0530

    maintained dates

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,24 @@
-export let cart=[{
+
+export const cart=JSON.parse(localStorage.getItem('cart'))||[{
   productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  quantity:1,
+  deliveryOptionId:'1',
+},
+{
+  productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
   quantity:2,
-}];
+  deliveryOptionId:'2',
+}
+];
+
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart))
+}
 
 export function updateCart(productId){
   let matchingitem;
     cart.forEach((item)=>{
-      if(item.id===productId){
+      if(item.productId===productId){
         matchingitem=item;
       }
     });
@@ -17,16 +29,18 @@ export function updateCart(productId){
       else{
         cart.push({
           productId:productId,
-          quantity:1
+          quantity:1,
+          deliveryOptionId : '1'
         });
       }
-      console.log(cart);
+      saveToStorage();
 }
 
 export function removefromCart(productId){
   cart.forEach((cartItem,index)=>{
     if(cartItem.productId===productId){
       cart.splice(index,1);
+      saveToStorage();
       return;
     }
   });
