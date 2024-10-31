c2671957dd16f8cad85b9dcec7cd38f16baa61db
jaseel_js <jazeel1132@gmail.com>
2024-10-31 21:25:33 +0530

    leant promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,23 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 
-loadProducts(()=>{
+ new Promise((resolve)=>{
+  loadProducts(()=>{
+    resolve();
+  }); 
+ }).then(()=>{
   renderOrderSummary();
   renderPaymentSummary();
+ });
+
+ /*
+ using call back function like below has some limitation if we have a lot of call back functions the code become more nested, 
+ so we use promise like above 'promise' so we can use the same purpose but code will not be nested but flat
+
+ promise.all let us use multiple promises (check out in video 20:54:00)
+ */ 
+/*
+loadProducts(()=>{
+  
 });
+*/
